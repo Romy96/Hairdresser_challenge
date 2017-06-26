d9cc9dedad7b6ac13fd43272106f9153a0a714d5
ROMY\Romy96 <romybijkerk1996@gmail.com>
2017-06-26 12:51:02 +0200

    Added relationships

--- hairdresser.sql
+++ hairdresser.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Gegenereerd op: 26 jun 2017 om 12:31
+-- Gegenereerd op: 26 jun 2017 om 12:50
 -- Serverversie: 5.6.17
 -- PHP-versie: 5.5.12
 
@@ -32,7 +32,9 @@ CREATE TABLE IF NOT EXISTS `agenda` (
   `start_time` time(6) DEFAULT NULL,
   `end_time` time(6) DEFAULT NULL,
   `hairdresser` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `date` (`date`),
+  KEY `hairdresser` (`hairdresser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -47,7 +49,10 @@ CREATE TABLE IF NOT EXISTS `appointments` (
   `hairdresser` int(11) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `date` (`date`),
+  KEY `hairdresser` (`hairdresser`),
+  KEY `customer` (`customer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -65,7 +70,8 @@ CREATE TABLE IF NOT EXISTS `customers` (
   `password` varchar(50) DEFAULT NULL,
   `email` varchar(250) DEFAULT NULL,
   `phone_number` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -83,8 +89,28 @@ CREATE TABLE IF NOT EXISTS `employees` (
   `password` varchar(50) DEFAULT NULL,
   `email` varchar(250) DEFAULT NULL,
   `phone_number` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `firstname` (`firstname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `agenda`
+--
+ALTER TABLE `agenda`
+  ADD CONSTRAINT `FK_agenda_employees` FOREIGN KEY (`hairdresser`) REFERENCES `employees` (`firstname`);
+
+--
+-- Beperkingen voor tabel `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `FK_appointments_agenda` FOREIGN KEY (`date`) REFERENCES `agenda` (`date`),
+  ADD CONSTRAINT `FK_appointments_customers` FOREIGN KEY (`customer`) REFERENCES `customers` (`id`),
+  ADD CONSTRAINT `FK_appointments_employees` FOREIGN KEY (`hairdresser`) REFERENCES `employees` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
